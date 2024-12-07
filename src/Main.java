b1e9bb255658c690b633fbd67be30a815927d48b
Даня <rigmen12311@gmail.com>
2024-12-08 00:22:58 +0300

    HW 9

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,55 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // task 1 //
+
+        int[] tally = {433521, 41154, 536234, 243095, 1};
+
+        int total =0;
+        for (int i = 0; i < tally.length; i++){
+            total += tally[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " руюблей");
+
+        // task 2 //
+
+        int min = tally[0];
+        int max = tally[1];
+
+
+        for (int i = 0; i < tally.length; i++) {
+            if (tally[i] < min){
+                min = tally[i];
+            }
+            if (tally[i] > max){
+                max = tally[i];
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " + max + " рублей");
+        System.out.println("Минимальная сумма трат за неделю составила " + min + " рублей");
+
+        // task 3 //
+
+        double total1 = (double) total / tally.length;
+        System.out.println("Средняя сумма трат за месяц составила " + total1 + " рублей");
+
+        // task 4 //
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+         int oneInedex = i;
+         int lastIdex = reverseFullName.length - i -1;
+
+         char tmp = reverseFullName[oneInedex];
+         reverseFullName[oneInedex] = reverseFullName[lastIdex];
+         reverseFullName[lastIdex] = tmp;
+        }
+        for (int i = 0; i < reverseFullName.length; i++) {
+            System.out.print(reverseFullName[i]);
+        }
+
 
 
 
